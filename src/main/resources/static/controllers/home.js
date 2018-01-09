56747dd7d4e9bd5dcab69b9c05ad3f80134025b1
valda-z <valdemar.zavadsky@microsoft.com>
2018-01-09 17:18:15 +0100

    initial commit

---
+++ src/main/resources/static/controllers/home.js
@@ -0,0 +1,57 @@
+'use strict';
+
+angular.module('myApp.home', ['ngRoute', 'ui.bootstrap'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+    });
+}])
+
+.controller('HomeCtrl', ['$scope', '$http', '$timeout', '$modal', function($scope, $http, $timeout, $modal) {
+
+    $scope.data = {
+        item: { comment: "", category: "" },
+        items: null,
+        comments: null,
+        isedit: false
+    };
+
+    $scope.loadData = function() {
+        myUtils.showPleaseWait();
+        $http.get('./api/todo/ToDoList').success(function(data) {
+            $scope.data.items = data;
+        }).finally(function(data) {
+            myUtils.hidePleaseWait();
+        });
+    };
+
+    $scope.addItem = function(item) {
+        $http.post('./api/todo/ToDoAdd', item).success(function(data) {}).finally(function(data) {
+            $scope.loadData();
+            $scope.data.item = { comment: "", category: "" };
+        });
+    };
+
+    $scope.editItem = function(item) {
+        $scope.data.isedit = true;
+        $scope.data.item = item;
+    };
+
+    $scope.editItemSave = function(item) {
+        $http.post('./api/todo/ToDo', item).success(function(data) {}).finally(function(data) {
+            $scope.data.isedit = false;
+            $scope.data.item = { comment: "", category: "" };
+            $scope.loadData();
+        });
+    };
+
+    $scope.editItemCancel = function(item) {
+        $scope.data.isedit = false;
+        $scope.data.item = { comment: "", category: "" };
+    };
+
+    $scope.loadData();
+
+}]);
